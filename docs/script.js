250abb3403f1eb61eafa64048d629cb3a87776ad
KoukiShimoya <shiomyameiji@gmail.com>
2018-04-23 21:11:19 +0900

    add js

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+var img = new Array("programingHTML.png","UnityHTML.png","shipHTML.png");
+var count=-1;
+imgTimer();
+
+function imgTimer(){
+    count++;
+    if(count==img.length){
+        count=0;
+    }
+    document.picture.src=img[count];
+    setTimeout("imgTimer()",5000);
+}

7360fa6fdf65bcbf48f2edf7f3ef57ffb864e4ac
KoukiShimoya <shiomyameiji@gmail.com>
2018-04-24 20:07:20 +0900

    add taskun

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,15 @@
-var img = new Array("programingHTML.png","UnityHTML.png","shipHTML.png");
-var count=-1;
-imgTimer();
+function imgChange() {
 
-function imgTimer(){
-    count++;
-    if(count==img.length){
-        count=0;
+    var img = new Array("programingHTML.png", "UnityHTML.png", "shipHTML.png");
+    var count = -1;
+    imgTimer();
+
+    function imgTimer() {
+        count++;
+        if (count == img.length) {
+            count = 0;
+        }
+        document.picture.src = img[count];
+        setTimeout("imgTimer()", 5000);
     }
-    document.picture.src=img[count];
-    setTimeout("imgTimer()",5000);
 }
